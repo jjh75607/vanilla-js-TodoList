286391733861ac8c50b0a02baeaa5da47b6d9874
jjh75607 <dkrak5738@naver.com>
2019-03-03 21:08:34 +0900

    classList

--- javaScript/condition.js
+++ javaScript/condition.js
@@ -21,24 +21,15 @@ false || false = false
 
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const currentColor = title.style.color;
-
-    if (currentColor === BASE_COLOR) {
-        title.style.color = OTHER_COLOR;
-    }
-    else {
-        title.style.color = BASE_COLOR;
-    }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);    
 }
-title.addEventListener("click", handleClick);
 
 init();
 
