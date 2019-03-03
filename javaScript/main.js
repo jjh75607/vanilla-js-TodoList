6b987daaf045ae7df4f4e84b92d9082d2f20b1d4
jjh75607 <dkrak5738@naver.com>
2019-03-03 18:44:23 +0900

    variable, array

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,2 +1,22 @@
 alert('hello, World');
 console.log('hello, World');
+
+//변수를 초기화 하거나 생성할때 let
+let a = 221;
+let b = a - 5; 
+console.log(a, b);
+
+//상수
+const c = 1;
+// c = 3; <-- error
+// 왠만하면 const 키워드를 사용하여 식별자를 선언하고, 값이 변하는 식발자일 경우 let 키워드를 이용하여 선언하자.
+
+//변수
+const string = "문자열";
+const boolean = true; //or false
+const number = 1;
+const float = 3.14;
+
+//배열 (값으로 변수, 숫자, 불리언 다 가능)
+const test = "test";
+const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun", 123, true, test];
