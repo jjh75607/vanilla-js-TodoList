841a5588fbcf9ae550f85bfed62fbfb2986452b7
jjh75607 <dkrak5738@naver.com>
2019-03-03 19:27:49 +0900

    javascript function

--- javaScript/main.js
+++ javaScript/main.js
@@ -3,7 +3,7 @@ console.log('hello, World');
 
 //변수를 초기화 하거나 생성할때 let
 let a = 221;
-let b = a - 5; 
+let b = a - 5;
 console.log(a, b);
 
 //상수
@@ -17,6 +17,49 @@ const boolean = true; //or false
 const number = 1;
 const float = 3.14;
 
+console.log(string, boolean, number, float)
+
 //배열 (값으로 변수, 숫자, 불리언 다 가능)
 const test = "test";
 const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun", 123, true, test];
+
+console.log(dayOfWeek[0]); //월요일
+
+//object, 안에 값으로 object or array도 가능
+const info = {
+    name: "name",
+    age: 999,
+    male: "male",
+    alive: true,
+};
+
+info.name = "soon jae";
+console.log(info.name);
+
+//function
+function hello(name) {
+    return `my name is ${name}`;
+}
+
+const myName = hello("soon jae");
+
+console.log(g);
+
+
+const calculator = {
+    plus: function(n1, n2)  {
+        return n1 + n2;
+    },
+    minus: function(n1, n2) {
+        return n1 - n2;
+    },
+    division: function(n1, n2) {
+        return n1 / n2;
+    },
+    multiply: function(n1, n2) {
+        return n1 * n2;
+    }
+}
+
+const result = calculator.plus(4, 3);
+console.log(result);
