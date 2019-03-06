37ca5417720e37bf5b84befd45027d526bf2c820
jjh75607 <dkrak5738@naver.com>
2019-03-06 20:55:42 +0900

    To Do List auto print

--- javaScript/todo.js
+++ javaScript/todo.js
@@ -4,27 +4,47 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
-function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
+const toDos = [];
 
-    if (toDos !== null) {
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
         
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        })
     }
+}
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
 
     delBtn.innerHTML = "X";
-
-    const span = document.createElement("span");
     span.innerText = text;
 
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
 
     toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+
+toDos.push(toDoObj);
+saveToDos();
+
 }
 
 function handleSubmit(event) {
